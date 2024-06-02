b0b3904ce06a0dcc9aa806f1adc4bcf65023e5a8
izabakker <138227173+Izaba0@users.noreply.github.com>
2024-06-02 23:23:15 +0200

    Added regex that limits to 255

--- recurseIP.ps1
+++ recurseIP.ps1
@@ -1,6 +1,7 @@
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 $directoryPath = "C:\Users\" # add path to directory 
-$ipPattern = '([0-9]{1,3}\.){3}[0-9]{1,3}'
+$ipPattern = '\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b'
+ # '([0-9]{1,3}\.){3}[0-9]{1,3}' is a much simpler regex but does not take into account the limit of 255
 
 Get-ChildItem -Path $directoryPath -Recurse -File | ForEach-Object {
     $file = $_.FullName
